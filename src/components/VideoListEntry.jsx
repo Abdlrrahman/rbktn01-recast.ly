b647cc2e8498ba1a003342c6a80f0c57f8655fe8
ghostrib <sh4rkbot@gmail.com>
2019-12-10 16:09:59 +0100

    solution

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,21 +1,28 @@
-var VideoListEntry = () => (
-  <div className="video-list-entry media">
-    <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
+var VideoPlayer = ({ video }) =>
+  !video ? (
+    <div className="video-player">Please wait...</div>
+  ) : (
+    <div className="video-player">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe
+          className="embed-responsive-item"
+          src={`https://www.youtube.com/embed/${video.id.videoId}`}
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div className="video-player-details">
+        <h3>{video.snippet.title}</h3>
+        <div>{video.snippet.description}</div>
+      </div>
     </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
-    </div>
-  </div>
-);
+  );
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
-VideoListEntry.propTypes = {
+VideoPlayer.propTypes = {
   video: React.PropTypes.object.isRequired
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
-export default VideoListEntry;
+export default VideoPlayer;
